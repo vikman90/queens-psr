2bca647f1cead6dc206adfe489af36f147ea5d9c
Vikman Fdez-Castro <vmfdez90@gmail.com>
2016-03-27 00:16:08 +0100

    JavaScript version

---
+++ JS/chess.js
@@ -0,0 +1,205 @@
+/*******************************************************************************
+ * N queens problem (JavaScript version)
+ * Class Chess
+ *
+ * Copyleft 2016 Vikman - All rights revoked.
+ * vikman90.blogspot.com - vmfdez90@gmail.com
+ * March 26, 2016
+ *
+ ******************************************************************************/
+
+function fill(length, value) {
+    var array = new Array(value)
+
+    for (var i = 0; i < length; i++)
+        array[i] = value
+
+    return array
+}
+
+function Chess(size) {
+
+    //--------------------------------------------------------------------------
+    // Constructor
+
+    var complete = fill(size, 1)
+
+    this.size = size
+    this.queens = new Array(size)
+    this.queensCount = fill(size, size)
+    this.nsteps = 0
+    this.stackDiscarded = []
+    this.stackCount = []
+
+    for (var i = 0; i < size; i++)
+        this.queens[i] = complete.slice()
+
+    //--------------------------------------------------------------------------
+    // Solve queens problem
+
+    this.solve = function() {
+        var index = this.selectIndex()
+
+        if (index == -1)
+            return true
+
+        var row = this.queens[index].slice()
+        var values = this.selectValues(row)
+
+        for (var i in values) {
+            var value = values[i]
+
+            if (!this.assign(index, value)) {
+                this.queens[index] = row.slice()
+                this.queensCount[index] = values.length
+                continue
+            }
+
+            if (this.solve())
+                return true
+
+            this.restoreLast()
+            this.queens[index] = row.slice()
+            this.queensCount[index] = values.length
+        }
+
+        return false
+    }
+
+    //--------------------------------------------------------------------------
+    // Get index of a unsolved row (minimum remaining values heuristics)
+
+    this.selectIndex = function() {
+        var minsize = this.size + 1
+        var index = -1
+
+        for (var i = 0; i < this.size; i++) {
+            var cursize = this.queensCount[i]
+
+            if (cursize > 1 && cursize < minsize) {
+                index = i
+                minsize = cursize
+            }
+        }
+
+        return index
+    }
+
+    //--------------------------------------------------------------------------
+    // Assign a value to a row and propagate constraints
+
+    this.assign = function(index, value) {
+        this.queens[index] = fill(this.size, 0)
+        this.queensCount[index] = 0
+        this.stackCount.push(0)
+        this.nsteps++
+
+        for (var i = 0; i < this.size; i++) {
+            if (i == index)
+                continue
+
+            var diag1 = value + (index - i)
+            var diag2 = value - (index - i)
+
+            if (!(this.discard(i, value) && this.discard(i, diag1) && this.discard(i, diag2))) {
+                this.restoreLast()
+                return false
+            }
+        }
+
+        this.queens[index][value] = 1
+        this.queensCount[index] = 1
+        return true
+    }
+
+    //--------------------------------------------------------------------------
+    // Discard candidate values (constraints propagation)
+
+    this.discard = function(index, value) {
+        if (value < 0 || value >= this.size || !this.queens[index][value])
+            return true
+
+        this.queens[index][value] = 0
+        this.queensCount[index]--
+        this.stackDiscarded.push([index, value])
+        this.stackCount[this.stackCount.length - 1]++
+
+        if (this.queensCount[index] == 0)
+            return false
+
+        if (this.queensCount[index] == 1) {
+            value = this.queens[index].indexOf(1)
+
+            for (var i = 0; i < this.size; i++) {
+                if (i == index)
+                    continue
+
+                var diag1 = value + (index - i)
+                var diag2 = value - (index - i)
+
+                if (!(this.discard(i, value) && this.discard(i, diag1) && this.discard(i, diag2)))
+                    return false
+            }
+        }
+
+        return true
+    }
+
+    //--------------------------------------------------------------------------
+    // Undo last assignation (restore constraints)
+
+    this.restoreLast = function() {
+        var n = this.stackCount.pop()
+
+        for (var i = 0; i < n; i++) {
+            var pair = this.stackDiscarded.pop()
+
+            if (!this.queens[pair[0]][pair[1]]) {
+                this.queens[pair[0]][pair[1]] = 1
+                this.queensCount[pair[0]]++
+            }
+        }
+    }
+
+    //--------------------------------------------------------------------------
+    // Select all available indices from a row
+
+    this.selectValues = function(row) {
+        var values = []
+        var offset = parseInt(Math.random() * this.size)
+
+        for (var i = 0; i < row.length; i++) {
+            var index = (offset + i) % this.size
+
+            if (row[index])
+                values.push(index)
+        }
+
+        return values
+    }
+
+    //--------------------------------------------------------------------------
+    // Representation of the chessboard
+
+    this.toString = function() {
+        var string = ""
+
+        for (var i = 0; i < this.size; i++) {
+            if (this.queensCount[i] != 1)
+                string += "Reina " + (i + 1) + " no resuelta\n"
+            else
+                string += "Reina " + (i + 1) + ": " + (this.queens[i].indexOf(1) + 1) + "\n"
+        }
+
+        return string
+    }
+
+    //--------------------------------------------------------------------------
+    // Get number of total tries of assignati
+
+    this.steps = function() {
+        return this.nsteps
+    }
+}
+
+module.exports.Chess = Chess
---
+++ JS/queens.js
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * N queens problem (JavaScript version)
+ * Main file
+ *
+ * Copyleft 2016 Vikman - All rights revoked.
+ * vikman90.blogspot.com - vmfdez90@gmail.com
+ * March 26, 2016
+ *
+ * Syntax: nodejs queens.js [-test] [N]
+ *
+ ******************************************************************************/
+
+const libchess = require("./chess")
+var n = 0
+testing = false
+
+for (i in process.argv)
+    if (process.argv[i] === "-test")
+        testing = true;
+    else
+        n = parseInt(process.argv[i])
+
+if (isNaN(n) || n < 4) {
+    console.error("Usage: " + process.argv[0] + " [-test] N")
+    process.exit(1);
+}
+
+var chess = new libchess.Chess(n)
+var hrtime = process.hrtime()
+var time = hrtime[0] + hrtime[1] / 1e9
+
+chess.solve()
+
+hrtime = process.hrtime()
+time = parseInt((hrtime[0] + hrtime[1] / 1e9 - time) * 1000)
+
+if (testing)
+    console.log(String(chess.steps()) + "\t" + time)
+else {
+    console.log(String(chess))
+    console.log("Resuelto en " + chess.steps() + " pasos. Tiempo: " + time + " ms.")
+}
