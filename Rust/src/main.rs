988451f89cb5bccee8696c89b6580fe06235270b
Vikman Fernandez-Castro <vmfdez90@gmail.com>
2024-02-23 16:10:43 +0100

    Add test flag to the Rust implementation

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -14,7 +14,7 @@ use solver::{Solver,SerialSolver, ParallelSolver};
 const DEFAULT_SIZE: usize = 100;
 
 fn main() {
-    let (size, parallel) = parse_args();
+    let (size, parallel, testing) = parse_args();
     let chess = Chess::new(size);
 
     let solver: &mut dyn Solver;
@@ -36,14 +36,15 @@ fn main() {
         panic!("Cannot resolve the problem");
     }
 
-    print_results(solver, now.elapsed());
+    print_results(solver, now.elapsed(), testing);
 
 }
 
 /// Parse arguments. Returns the chess size and the parallel mode
-fn parse_args() -> (usize, bool) {
+fn parse_args() -> (usize, bool, bool) {
     let mut size = DEFAULT_SIZE;
     let mut parallel = false;
+    let mut testing = false;
 
     for arg in env::args() {
         if arg == "-h" || arg == "--help" {
@@ -51,20 +52,26 @@ fn parse_args() -> (usize, bool) {
             exit(0);
         } else if arg == "-p" || arg == "--parallel" {
             parallel = true;
+        } else if arg == "-test" {
+            testing = true;
         } else if let Ok(v) = arg.parse::<usize>() {
             size = v;
         }
     }
 
-    (size, parallel)
+    (size, parallel, testing)
 }
 
 /// Print algorithm results into stdout and stderr.
-fn print_results(solver: &dyn Solver, duration: Duration) {
-    print!("{}", solver.chess());
-    eprintln!("Trials:      {}", solver.trials());
-    eprintln!("Discards:    {}", solver.discards());
-    eprintln!("Time:        {:.3} ms.", duration.as_millis());
-    eprintln!("Performance: {:.3} steps/μs.", solver.trials() as f64 / duration.as_micros() as f64);
-    eprintln!("             {:.3} discards/μs.", solver.discards() as f64 / duration.as_micros() as f64);
+fn print_results(solver: &dyn Solver, duration: Duration, testing: bool) {
+    if testing {
+        println!("{}\t{}\t{}", solver.trials(), solver.discards(), duration.as_micros());
+    } else {
+        print!("{}", solver.chess());
+        eprintln!("Trials:      {}", solver.trials());
+        eprintln!("Discards:    {}", solver.discards());
+        eprintln!("Time:        {:.3} ms.", duration.as_millis());
+        eprintln!("Performance: {:.3} steps/μs.", solver.trials() as f64 / duration.as_micros() as f64);
+        eprintln!("             {:.3} discards/μs.", solver.discards() as f64 / duration.as_micros() as f64);
+    }
 }
