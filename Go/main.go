cd811400d7e47b31070c14e0bd756b7626f6c5ea
Vikman Fernandez-Castro <vmfdez90@gmail.com>
2024-02-23 15:30:49 +0100

    Add test flag to the Go implementation

--- Go/main.go
+++ Go/main.go
@@ -4,8 +4,8 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"math/rand"
 	"os"
 	"strconv"
 	"time"
@@ -14,19 +14,23 @@ import (
 )
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
 	size := 4
+	testing := false
 
-	if len(os.Args) > 1 {
-		s, err := strconv.Atoi(os.Args[1])
+	for _, arg := range os.Args[1:] {
+		if arg == "-test" {
+			testing = true
+		} else {
+			s, err := strconv.Atoi(arg)
 
-		if err != nil {
-			log.Fatal(err)
-		} else if size < 4 {
-			log.Fatal("The chess size must be greater or equal than 4.")
+			if err != nil {
+				log.Fatal(err)
+			} else if size < 4 {
+				log.Fatal("The chess size must be greater or equal than 4.")
+			}
+
+			size = s
 		}
-
-		size = s
 	}
 
 	chess := chess.New(size)
@@ -35,10 +39,14 @@ func main() {
 	chess.Solve()
 	end := time.Now()
 
-	chess.Print()
-	log.Println("Steps:      ", chess.GetSteps())
-	log.Println("Discards:   ", chess.GetDiscards())
-	log.Println("Time:       ", end.Sub(start))
-	log.Printf("Performance: %d steps/ms\n", int64(chess.GetSteps()*1000000)/end.Sub(start).Nanoseconds())
-	log.Printf("             %d discards/ms\n", int64(chess.GetDiscards()*1000000)/end.Sub(start).Nanoseconds())
+	if testing {
+		fmt.Printf("%d\t%d\t%d\n", chess.GetSteps(), chess.GetDiscards(), end.Sub(start).Microseconds())
+	} else {
+		chess.Print()
+		fmt.Println("Steps:      ", chess.GetSteps())
+		fmt.Println("Discards:   ", chess.GetDiscards())
+		fmt.Println("Time:       ", end.Sub(start))
+		fmt.Printf("Performance: %d steps/ms\n", int64(chess.GetSteps()*1000000)/end.Sub(start).Nanoseconds())
+		fmt.Printf("             %d discards/ms\n", int64(chess.GetDiscards()*1000000)/end.Sub(start).Nanoseconds())
+	}
 }
